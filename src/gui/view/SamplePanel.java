926f93f8da0535e4e1cc2dd483645a673d315209
Ian Erickson <19ierickson@gmail.com>
2018-11-27 12:11:05 -0700

    created SamplePanel class

---
+++ src/gui/view/SamplePanel.java
@@ -0,0 +1,18 @@
+package gui.view;
+
+import javax.swing.*;
+import gui.controller.GuiController;
+
+public class SamplePanel extends JPanel
+{
+	private GuiController appController;
+	
+	public SamplePanel(GuiController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+	}
+	
+}
