1e78a7084b859bc764b50377d92cccab78bc52d5
Ian Erickson <19ierickson@gmail.com>
2018-11-27 12:06:48 -0700

    added the SampleFrame class

---
+++ src/gui/view/SampleFrame.java
@@ -0,0 +1,28 @@
+package gui.view;
+
+import javax.swing.JFrame;
+import gui.controller.GuiController;
+
+public class SampleFrame extends JFrame
+{
+	private GuiController appController;
+	private SamplePanel appPanel;
+	
+	public SampleFrame(GuiController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("Le GUI");
+		this.setSize(1000, 500);
+		this.setResizable(true);
+	}
+}
