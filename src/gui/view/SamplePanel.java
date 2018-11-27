3f4362ec741e23ae2dcd436c92504a234f5c96de
Ian Erickson <19ierickson@gmail.com>
2018-11-27 12:52:39 -0700

    Made it change colors and display text

--- src/gui/view/SamplePanel.java
+++ src/gui/view/SamplePanel.java
@@ -2,6 +2,9 @@ package gui.view;
 
 import javax.swing.*;
 import gui.controller.GuiController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class SamplePanel extends JPanel
 {
@@ -25,6 +28,7 @@ public class SamplePanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setBackground(Color.LIGHT_GRAY);
 		this.add(textLabel);
 		this.add(colorButton);
 	}
@@ -36,6 +40,22 @@ public class SamplePanel extends JPanel
 	
 	private void setupListeners()
 	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeBackgroundColor();
+			}
+		});
+	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
 		
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
 	}
 }
