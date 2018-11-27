7d165ae21d4d9d94321aedd0e98b37f93964e838
Ian Erickson <19ierickson@gmail.com>
2018-11-27 12:21:08 -0700

    finished up controller

--- src/gui/controller/GuiController.java
+++ src/gui/controller/GuiController.java
@@ -1,7 +1,15 @@
 package gui.controller;
 
+import gui.view.SampleFrame;
+
 public class GuiController
 {
+	private SampleFrame appFrame;
+	
+	public GuiController()
+	{
+		appFrame = new SampleFrame(this);
+	}
 	
 	public void start()
 	{
